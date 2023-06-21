9d02b318506c09ac81513f2e60073b5af6e0377e
trieuld <letrieu106@gmail.com>
2023-06-21 21:20:43 +0700

    dockerfile

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	print("Hello Docker")
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{"code": 200})
+	})
+
+	router.Run(":8080")
 }
